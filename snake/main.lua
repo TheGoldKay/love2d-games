cbacc2f5234d378045cb884122f860f1463232b6
TheGoldKay <jhonnatanwfc@live.com>
2022-11-06 03:10:28 -0300

    snake move

--- snake/main.lua
+++ snake/main.lua
@@ -7,3 +7,33 @@ end
 function love.draw()
     snake:draw()
 end
+
+function love.keypressed(key)
+    if key == 'w'and snake.vertical == false then 
+        snake.yvel = -snake.vel
+        snake.xvel = 0 
+        snake.vertical = true 
+        snake.horizontal = false
+    elseif key == 's'and snake.vertical == false then 
+        snake.yvel = snake.vel 
+        snake.xvel = 0
+        snake.vertical = true
+        snake.horizontal = false 
+    elseif key == 'a' and snake.horizontal == false then 
+        snake.xvel = -snake.vel
+        snake.yvel = 0
+        snake.horizontal = true 
+        snake.vertical = false 
+    elseif key == 'd' and snake.horizontal == false then 
+        snake.xvel = snake.vel 
+        snake.yvel = 0
+        snake.horizontal = true 
+        snake.vertical = false
+    elseif key == 'escape' then 
+        love.event.quit()
+    end
+end 
+
+function love.update(dt)
+    snake:move(dt)
+end
--- snake/snake.lua
+++ snake/snake.lua
@@ -3,18 +3,59 @@ Snake.__index = Snake
 
 function Snake:init()
     self = setmetatable({}, self)
+    self.box = 20
+    self.xmax = love.graphics.getWidth() / self.box 
+    self.ymax = love.graphics.getHeight() / self.box 
     self.body = {}
     head = {}
-    head.x = love.graphics.getWidth() / 2
-    head.y = love.graphics.getHeight() / 2
-    head.size = 20
+    head.x = self.xmax / 2
+    head.y = self.ymax / 2
+    self.vel = 1
+    self.xvel = -self.vel
+    self.yvel = 0
+    self.s = 20
+    self.timer = 0.2
+    self.clock = 0
+    self.horizontal = true 
+    self.vertical = false 
     table.insert(self.body, head)
+    x = head.x 
+    for i = 1, 5, 1 do 
+        part = {}
+        part.x = head.x + i 
+        part.y = head.y 
+        table.insert(self.body, part)
+    end 
     return self 
 end 
 
 function Snake:draw()
     for k, part in pairs(self.body) do 
-        love.graphics.rectangle('fill', part.x, part.y, part.size, part.size)
+        x = self.box * part.x 
+        y = self.box * part.y 
+        love.graphics.rectangle('fill', x, y, self.s, self.s)
+    end 
+end 
+
+function Snake:move(dt)
+    self.clock = self.clock + dt 
+    if self.clock > self.timer then 
+        self.clock = 0
+        local head = self.body[1]
+        head.x = head.x + self.xvel 
+        head.y = head.y + self.yvel 
+        if head.x <= 0 then 
+            head.x = self.xmax
+        elseif head.x >= self.xmax then 
+            head.x = 0
+        end 
+        if head.y <= 0 then 
+            head.y = self.ymax
+        elseif head.y >= self.ymax then 
+            head.y = 0
+        end 
+        table.insert(self.body, 1, head)
+        table.remove(self.body, #self.body)
     end 
 end 
 
