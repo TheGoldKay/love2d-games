06858db2a55251dda2d310ee4347dbcd4307d851
TheGoldKay <jhonnatanwfc@live.com>
2022-11-07 02:34:28 -0300

    ball

--- breakout/main.lua
+++ breakout/main.lua
@@ -2,6 +2,10 @@ Pallet = require('pallet')
 
 function love.load()
     pallet = Pallet:init()
+    ball = {}
+    ball.x = love.graphics.getWidth() / 2
+    ball.y = love.graphics.getHeight() / 2
+    ball.r = 20
 end 
 
 function love.keypressed(key)
@@ -12,6 +16,7 @@ end
 
 function love.draw()
     pallet:draw()
+    love.graphics.circle('line', ball.x, ball.y, ball.r)
 end 
 
 function love.update(dt)
