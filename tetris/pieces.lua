af4a661e32bda508a212e07f71c9e12821615181
Pyro Ruby <jhonnatanwfc@live.com>
2022-11-23 10:04:03 -0300

    draw grid

--- tetris/pieces.lua
+++ tetris/pieces.lua
@@ -7,12 +7,12 @@ shapes["line"] = {{0, 0}, {1, 0}, {2, 0}, {3, 0}}
 
 function Pieces:new()
     self = setmetatable({}, self)
-    self.actual = shapes.square;
+    self.actual = shapes.line;
     self.size = 40;
     self.nrow = win_h / self.size 
     self.ncol = win_w / self.size 
     self.row = 0
-    self.col = self.ncol / 2
+    self.col = math.floor(self.ncol / 2)
     return self 
 end 
 
@@ -23,6 +23,15 @@ function Pieces:draw()
         y = (y + self.row ) * self.size 
         love.graphics.rectangle('fill', x, y, self.size, self.size)
     end 
+    love.graphics.setColor(0, 0, 1)
+    for row = 0, self.nrow do 
+        for col = 0, self.ncol do 
+            y = row * self.size 
+            x = col * self.size 
+            love.graphics.rectangle('line', x, y, self.size, self.size)
+        end 
+    end
+    love.graphics.setColor(1,1,1)
 end
 
 return Pieces
