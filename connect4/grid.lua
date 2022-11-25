66b6bcb111020f2f1c9c799ee95c04c7a3051af7
TheGoldKay <jhonnatanwfc@live.com>
2022-11-25 19:18:25 -0300

    cell rect circle

--- connect4/grid.lua
+++ connect4/grid.lua
@@ -7,19 +7,39 @@ function Grid:new()
     self.row = 6 
     self.col = 6
     self.size = ((love.graphics.getWidth() / 4) / 6) * 2
+    self:create_grid()
     return self
 end
 
-function Grid:draw()
+function Grid:create_grid()
+    self.list = {}
     for r = 0, self.row - 1 do 
         for c = 0, self.col - 1 do 
+            local rect = {}
+            local circle = {}
+            local cell = {}
             local x = c * self.size + self.gap
             local y = r * self.size + self.gap
-            love.graphics.rectangle('line', x, y, self.size, self.size)
+            rect.x = x 
+            rect.y = y
+            cell.rect = rect 
+            --love.graphics.rectangle('line', x, y, self.size, self.size)
             local cx = x + self.size / 2 
             local cy = y + self.size / 2 
-            love.graphics.circle('line', cx, cy, self.size/2)
+            circle.x = cx 
+            circle.y = cy 
+            circle.mode = 'line'
+            cell.circle = circle 
+            --love.graphics.circle('line', cx, cy, self.size/2)
+            table.insert(self.list, cell)
         end 
+    end 
+end 
+
+function Grid:draw()
+    for _, cell in pairs(self.list) do 
+        love.graphics.rectangle('line', cell.rect.x, cell.rect.y, self.size, self.size)
+        love.graphics.circle(cell.circle.mode, cell.circle.x, cell.circle.y, self.size / 2)
     end 
 end 
 
