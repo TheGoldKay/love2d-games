d8c31c8cc420a6f81d160d1960f12638f70e6ffc
Ruby <jhonnatanwfc@live.com>
2023-06-03 16:05:37 -0300

    hit the circle

--- dot_poker/main.lua
+++ dot_poker/main.lua
@@ -1,14 +1,28 @@
 function love.load()
-    circle = {}
-    circle.x, circle.y = randPoint()
-    circle.r = 40 -- radius
-    circle.color = {0, .5, .5}
+    circle = make_circle()
     love.graphics.setBackgroundColor(.15, .15, .15)
 end
 
 function love.keypressed(key)
-    if(key == 'escape') then 
+    if (key == 'escape') then 
         love.event.quit()
+    end
+end
+
+function make_circle()
+    circle = {}
+    circle.x, circle.y = randPoint()
+    circle.r = 40 -- radius
+    local red, green, blue = love.math.random(), love.math.random(), love.math.random()
+    circle.color = {red, green, blue}
+    return circle 
+end
+
+function love.mousepressed(x, y, button)
+    if (button == 1) then 
+        if (math.sqrt(math.pow(x - circle.x, 2) + math.pow(y - circle.y, 2)) < circle.r) then 
+            make_circle()
+        end
     end
 end
 
@@ -21,4 +35,6 @@ end
 function love.draw()
     love.graphics.setColor(circle.color)
     love.graphics.circle('fill', circle.x, circle.y, circle.r)
+    love.graphics.setColor({1, 1, 1}) -- white
+    love.graphics.circle('line', circle.x, circle.y, circle.r+1)
 end
