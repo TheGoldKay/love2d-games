562370deada71d74cd0d4f6914f56195ec97efdd
Ruby <jhonnatanwfc@live.com>
2023-06-07 19:25:50 -0300

    fixing explosion animation

--- dot_poker/main.lua
+++ dot_poker/main.lua
@@ -1,11 +1,15 @@
-require "tesound"
-
 function love.load()
+    require "libs/tesound"
+    anim8 = require "libs/anim8"
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    quad_w, quad_h = 100, 100
     circle = make_circle()
-    explosion = newAnimation(love.graphics.newImage("explosion.png"), 100, 100, 0.3)
+    explosion_img = love.graphics.newImage("art/explosion.png")
+    local explosion_grid = anim8.newGrid(quad_w, quad_h, explosion_img:getWidth(), explosion_img:getHeight())
+    explosion_anim = anim8.newAnimation(explosion_grid("1-10", 2), 0.05)
     love.graphics.setBackgroundColor(0.15, 0.15, 0.15)
-    mousex, mousey = 0, 0
-    hit = false 
+    hit_clock = 0
+    hit_timer = 0.3
 end
 
 function love.keypressed(key)
@@ -44,16 +48,16 @@ function love.mousepressed(x, y, button)
     if (button == 1) then 
         if (math.sqrt(math.pow(x - circle.x, 2) + math.pow(y - circle.y, 2)) < circle.r) then 
             TEsound.play("sounds/DeathFlash.flac", "static")
-            hit = true
-            mousex, mousey = x, y
-            make_circle()
+            hit_clock = hit_timer
+            exp_x, exp_y = circle.x - quad_w, circle.y - quad_h * 2 + circle.r
+            explosion_anim = explosion_anim:clone()
         end
     end
 end
 
 function randPoint()
-    x = love.math.random(WIDTH)
-    y = love.math.random(HEIGHT)
+    x = love.math.random(quad_w, WIDTH - quad_w)
+    y = love.math.random(quad_h, HEIGHT - quad_h)
     return x, y
 end
 
@@ -62,19 +66,20 @@ function love.draw()
     love.graphics.circle('fill', circle.x, circle.y, circle.r)
     love.graphics.setColor({1, 1, 1}) -- white
     love.graphics.circle('line', circle.x, circle.y, circle.r+1)
-    if (hit) then 
-        local spriteNum = math.floor(explosion.currentTime / explosion.duration * #explosion.quads) + 1
-        love.graphics.draw(explosion.spriteSheet, explosion.quads[spriteNum], mousex, mousey, 0, 2)
-        if(spriteNum == #explosion.quads) then 
-            hit = false 
-        end 
+    if(hit_clock > 0) then 
+        explosion_anim:draw(explosion_img, exp_x, exp_y, 0, 2)
+    end
+    if(hit_clock < 0) then 
+        make_circle()
     end 
 end
 
 function love.update(dt)
     TEsound.cleanup()
-    explosion.currentTime = explosion.currentTime + dt
-    if explosion.currentTime >= explosion.duration then
-        explosion.currentTime = explosion.currentTime - explosion.duration
-    end
+    explosion_anim:update(dt)
+    if(hit_clock > 0) then 
+        hit_clock = hit_clock - dt 
+    else 
+        hit_clock = 0
+    end 
 end
