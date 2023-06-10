2a919b1ab991d26140563fe6a3e9f76549c53c45
Ruby <jhonnatanwfc@live.com>
2023-06-10 14:21:52 -0300

    move tiles

--- fifteen/main.lua
+++ fifteen/main.lua
@@ -1,14 +1,8 @@
 function love.load()
-    love.graphics.setDefaultFilter("nearest", "nearest")
     cell = makeCell()
     grid = makeGrid(4, 4)
     font = makeFont()
-end
-
-function love.keypressed(key)
-    if(key == "escape") then 
-        love.event.quit()
-    end
+    blank = {4, 4} -- the number 16 (last one is black) (row, col)
 end
 
 function makeFont()
@@ -46,17 +40,56 @@ function randColor()
     return {r, g, b}
 end
 
+function love.keypressed(key)
+    if(key == "escape") then 
+        love.event.quit()
+    end
+    local row, col = blank[1], blank[2]
+    if(key == 's') then 
+        if(row - 1 >= 1) then 
+            local num = grid[row - 1][col]
+            grid[row][col] = num 
+            grid[row - 1][col] = 16 
+            blank = {row - 1, col}
+        end
+    elseif(key == 'w') then 
+        if(row + 1 <= 4) then 
+            local num = grid[row + 1][col]
+            grid[row][col] = num 
+            grid[row + 1][col] = 16
+            blank = {row + 1, col}
+        end
+    elseif(key == 'a') then 
+        if(col + 1 <= 4) then 
+            local num = grid[row][col + 1]
+            grid[row][col] = num 
+            grid[row][col + 1] = 16 
+            blank = {row, col + 1}
+        end
+    elseif(key == 'd') then 
+        if(col - 1 >= 1) then 
+            local num = grid[row][col - 1]
+            grid[row][col] = num 
+            grid[row][col - 1] = 16 
+            blank = {row, col - 1}
+        end
+    end
+end
+
+
 function love.draw()
     for row, line in ipairs(grid) do 
         for col, num in ipairs(line) do 
             local y = (row - 1) * cell.size
             local x = (col - 1) * cell.size
-            love.graphics.setColor(cell.color)
-            love.graphics.rectangle("fill", x, y, cell.size, cell.size)
+            if(num ~= 16) then 
+                love.graphics.setColor(cell.color)
+                love.graphics.rectangle("fill", x, y, cell.size, cell.size)
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.print(num, font.font, x + cell.size / 2 - font.size / 2, y + cell.size / 2 - font.size / 2)
+            end
             love.graphics.setColor(0, 0, 0)
             love.graphics.rectangle("line", x, y, cell.size, cell.size)
-            love.graphics.setColor(1, 1, 1)
-            love.graphics.print(num, font.font, x + cell.size / 2 - font.size / 2, y + cell.size / 2 - font.size / 2)
         end
     end
 end
