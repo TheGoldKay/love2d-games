3a04764031f8e527aa3ce4dd82231125dd9d59fe
Jhonnatan Carvalho <jhonnatanwfc@live.com>
2023-12-20 15:49:42 -0300

    bullets

--- typing_warrior/main.lua
+++ typing_warrior/main.lua
@@ -10,6 +10,9 @@ function love.load()
     ship.img = love.graphics.newImage("assets/Emissary.png")
     ship.x = settings.window.width / 2 - ship.img:getWidth() / 2
     ship.y = settings.window.height  - ship.img:getHeight() * 2
+    bullets = {} -- list of bullets (fired)
+    bullets.img = love.graphics.newImage("assets/BeholderBullets.png")
+    bullets.list = {} -- no bullets fired yet
     p = {} -- particle object
     p.r = 5 -- the individual particle's radius
     p.adjust = 10
@@ -22,14 +25,21 @@ end
 function love.draw()
     love.graphics.draw(bg.img, 0, bg.y)
     love.graphics.draw(ship.img, ship.x, ship.y)
-    love.graphics.setColor(1, 1, 1)
+    --love.graphics.setColor(1, 1, 1)
     --love.graphics.circle("fill", ship.x + ship.img:getWidth() / 2 + p.adjust, ship.y + ship.img:getHeight() - p.adjust, p.r)
     --love.graphics.circle("fill", ship.x + ship.img:getWidth() / 2 - p.adjust, ship.y + ship.img:getHeight() - p.adjust, p.r)
+    if #bullets.list > 0 then
+        for i = 1, #bullets.list do
+            love.graphics.draw(bullets.img, bullets.list[i].x, bullets.list[i].y)
+        end
+    end
 end
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
+    elseif key == "space" then 
+        table.insert(bullets.list, {x = ship.x + ship.img:getWidth() / 4, y = ship.y})
     end
 end
 
@@ -37,5 +47,14 @@ function love.update(dt)
     bg.y = bg.y + bg.vel * dt
     if bg.y > bg.img:getHeight() then
         bg.y = -bg.img:getHeight()
+    end
+    if #bullets.list > 0 then
+        for i = 1, #bullets.list do
+            bullets.list[i].y = bullets.list[i].y - 10
+            if bullets.list[i].y < 0 then
+                table.remove(bullets.list, i)
+                break
+            end
+        end
     end
 end
