83a2595fd14282a8db1754c14a9a9c50fb3adf1b
Jhonnatan Carvalho <jhonnatanwfc@live.com>
2023-12-20 16:43:44 -0300

    Pseudo infinite background

--- typing_warrior/main.lua
+++ typing_warrior/main.lua
@@ -3,7 +3,11 @@ local settings = require "conf"
 function love.load()
     --love.graphics.setBackgroundColor(rbg(unpack(settings.window.bg_color)))
     bg = {} -- background object
-    bg.img = love.graphics.newImage("assets/spacebg.png")
+    -- index start at 1 in Lua
+    bg.index = 1 -- there are currently seven long (3000 pixels) backgrounds to create the illusion of movement
+    -- once it reaches the end of the screen, load the next background
+    bg.img = love.graphics.newImage(nextBg())
+    bg.prev = {} --- table with the previous background img and position
     bg.y = -bg.img:getHeight() + settings.window.height
     bg.vel = 10
     ship = {} -- ship object
@@ -13,9 +17,17 @@ function love.load()
     bullets = {} -- list of bullets (fired)
     bullets.img = love.graphics.newImage("assets/BeholderBullets.png")
     bullets.list = {} -- no bullets fired yet
-    p = {} -- particle object
-    p.r = 5 -- the individual particle's radius
-    p.adjust = 10
+end
+
+function nextBg()
+    if bg.index <= 7 then 
+        local bgImg = string.format("assets/bg%d.png", bg.index)
+        bg.index = bg.index + 1
+        return bgImg    
+    else 
+        bg.index = 1
+        return string.format("assets/bg%d.png", bg.index)
+    end
 end
 
 function rbg(r, g, b)
@@ -24,10 +36,10 @@ end
 
 function love.draw()
     love.graphics.draw(bg.img, 0, bg.y)
+    if next(bg.prev) then
+        love.graphics.draw(bg.prev.img, 0, bg.prev.y)
+    end
     love.graphics.draw(ship.img, ship.x, ship.y)
-    --love.graphics.setColor(1, 1, 1)
-    --love.graphics.circle("fill", ship.x + ship.img:getWidth() / 2 + p.adjust, ship.y + ship.img:getHeight() - p.adjust, p.r)
-    --love.graphics.circle("fill", ship.x + ship.img:getWidth() / 2 - p.adjust, ship.y + ship.img:getHeight() - p.adjust, p.r)
     if #bullets.list > 0 then
         for i = 1, #bullets.list do
             love.graphics.draw(bullets.img, bullets.list[i].x, bullets.list[i].y)
@@ -45,8 +57,13 @@ end
 
 function love.update(dt)
     bg.y = bg.y + bg.vel * dt
-    if bg.y > bg.img:getHeight() then
-        bg.y = -bg.img:getHeight()
+    if bg.y >= 0 then
+        --- set the currently bg (now previous) to still be displayed till its full length
+        bg.prev.img = bg.img
+        bg.prev.y = bg.y 
+        bg.img = love.graphics.newImage(nextBg())
+        bg.y = -bg.img:getHeight() + settings.window.height
+        print(bg.index)
     end
     if #bullets.list > 0 then
         for i = 1, #bullets.list do
@@ -55,6 +72,12 @@ function love.update(dt)
                 table.remove(bullets.list, i)
                 break
             end
+        end
+    end
+    if next(bg.prev) then 
+        bg.prev.y = bg.prev.y + bg.vel * dt
+        if bg.prev.y >= settings.window.height then 
+            bg.prev = {}
         end
     end
 end
