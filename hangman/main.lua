9c808df374c272e79e1d7ca8e1a2f1b6b506fcfb
TheGoldKay <jhonnatanwfc@live.com>
2024-02-25 15:57:47 -0300

    fix/set API response

--- hangman/main.lua
+++ hangman/main.lua
@@ -1,5 +1,6 @@
 local http = require("socket.http")
 local ltn12 = require("ltn12")
+local json = require("libs/json")
 
 function get_word(word_len, category)
     word_len = word_len or 4
@@ -19,12 +20,21 @@ function get_word(word_len, category)
 
     -- Check if the request was successful
     if status_code == 200 then
-        -- Convert the response body table to a string
-        local body = table.concat(response_body)
-        return body
+        local response = json.decode(response_body[1])
+        local index = math.random(1, response_size(response))
+        local res = response[index]
+        return {word = res["word"], category = res["category"], hint = res["hint"], len = res["numLetters"]}
     else
-        error("Error: " ..status_code)
+        error("Error: " .. status_code)
     end
+end
+
+function response_size(response)
+    local count = 0
+    for _ in pairs(response) do
+        count = count + 1
+    end
+    return count 
 end
 
 function love.load()
@@ -36,11 +46,14 @@ function love.load()
     love.graphics.setBackgroundColor(color("#00c04b"))
     categories = {"animal", "sports", "fruit", "tool"}
     local word = get_word(4, "animal")
-    print(word)
+    print(word.word, word.category, word.hint, word.len)
 end
 
 function color(hex, value)
-	return {tonumber(string.sub(hex, 2, 3), 16)/256, tonumber(string.sub(hex, 4, 5), 16)/256, tonumber(string.sub(hex, 6, 7), 16)/256, value or 1}
+	return {tonumber(string.sub(hex, 2, 3), 16)/256, 
+            tonumber(string.sub(hex, 4, 5), 16)/256, 
+            tonumber(string.sub(hex, 6, 7), 16)/256, 
+            value or 1}
 end
 
 function love.keypressed(key)
