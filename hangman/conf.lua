9a07e42c2475670da0d4565d17abe43abae89270
TheGoldKay <jhonnatanwfc@live.com>
2024-02-25 15:29:38 -0300

    setting up word API

--- hangman/conf.lua
+++ hangman/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.version = "11.5"
-    --t.console = true
+    t.console = true
     t.window.title = "Hangman"
     t.window.width = 800
     t.window.height = 400
--- hangman/main.lua
+++ hangman/main.lua
@@ -1,3 +1,32 @@
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+
+function get_word(word_len, category)
+    word_len = word_len or 4
+    category = category or "animal"
+    -- URL of the API endpoint
+    local url = "https://www.wordgamedb.com/api/v1/words/?category=" .. category .. "&numLetters=" .. word_len
+
+    -- Table to store the response body
+    local response_body = {}
+
+    -- Make the API request
+    local res, status_code, response_headers = http.request{
+        url = url,
+        method = "GET", -- Change this to "POST", "PUT", etc. if needed,
+        sink = ltn12.sink.table(response_body)
+    }
+
+    -- Check if the request was successful
+    if status_code == 200 then
+        -- Convert the response body table to a string
+        local body = table.concat(response_body)
+        return body
+    else
+        error("Error: " ..status_code)
+    end
+end
+
 function love.load()
     local _, _, flags = love.window.getMode()
     local width, height = love.window.getDesktopDimensions(flags.display)
@@ -5,6 +34,9 @@ function love.load()
     local x, y = width / 2 - game_w / 2, height * 0.1
     love.window.setPosition(x, y)
     love.graphics.setBackgroundColor(color("#00c04b"))
+    categories = {"animal", "sports", "fruit", "tool"}
+    local word = get_word(4, "animal")
+    print(word)
 end
 
 function color(hex, value)
