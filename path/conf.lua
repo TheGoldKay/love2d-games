3f95aff080b3da31f38beeef01ceb3e01d3da90b
TheGoldKay <jhonnatanwfc@live.com>
2024-04-21 11:21:44 -0300

    random path

--- path/conf.lua
+++ path/conf.lua
@@ -1,5 +1,13 @@
+local settings = {
+    win_w = 400,
+    win_h = 700,
+    box_size = 40,
+}
+
 function love.conf(t)
     t.window.title = "Path"
-    t.window.width = 400
-    t.window.height = 700
+    t.window.width = settings.win_w
+    t.window.height = settings.win_h
 end
+
+return settings
--- path/main.lua
+++ path/main.lua
@@ -1,5 +1,34 @@
+local helper = require "love2d-helper/helper"
+local settings = require "conf"
+
+function love.load()
+    colors = helper:colors()
+    love.graphics.setBackgroundColor(colors.phthalo_green)
+    range_x = {settings.win_w / 2 - settings.box_size, settings.win_w / 2 + settings.box_size}
+    path = trace_path(20)
+end
+
+function trace_path(count)
+    local path = {}
+    for i = 1, count do 
+        x = math.random(range_x[1], range_x[2])
+        y = settings.win_h - settings.box_size * i
+        table.insert(path, {x, y})
+    end
+    return path 
+end
+
+function love.draw()
+    for k, v in pairs(path) do
+        love.graphics.rectangle("fill", v[1], v[2], settings.box_size  * 2, settings.box_size)
+    end
+end
+
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
+    end
+    if key == "space" then
+        path = trace_path(20)
     end
 end
